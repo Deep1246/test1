ccfd71c38b72bace5781fcb07c1b6170a012b85d
sandeep <sandeep314yadav@gmail.com>
2023-06-28 17:49:29 +0530

    first commit

---
+++ src/src/test/test1.java
@@ -0,0 +1,15 @@
+package src.test;
+
+public class test1 {
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		
+		if(hungry=="yes") {
+			System.out.println("hungry to much");
+		}else {
+			System.out.println("No no that much ungry");
+		}
+	}
+	
+}
